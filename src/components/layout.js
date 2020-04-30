d271d1e1fbf9b0ae95ca9727f52529a75032a4f7
W. Clark Newell <clark@clarknewell.tech>
2020-04-30 07:50:52 -0600

    Connecting GitHub Desktop
    Connecting GitHub Desktop

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,20 +23,22 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '8em',
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <main>{children}
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </main>
       </div>
     </>
   )
