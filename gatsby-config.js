61dc129c194b0946f56d62d138ef4c7a471a7bf3
W. Clark Newell <clark@clarknewell.tech>
2020-05-01 19:16:17 -0600

    Minor updates to package and config
    Minor updates to package and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `gatsby-parallax-stars`,
+    description: `A gatsby sandbox app for experimenting with Sass.`,
     author: `@gatsbyjs`,
   },
   plugins: [
