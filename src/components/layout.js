f1e421fcea1a121951874cfa7104c58558be5a26
W. Clark Newell <clark@clarknewell.tech>
2020-05-15 07:20:43 -0600

    New Logo
    New Logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Icon from "./assets/astronautClark.inline.svg"
+import Icon from "./assets/OG-Astronaut.inline.svg"
 import "./layout.sass"
 
 const Layout = () => {
